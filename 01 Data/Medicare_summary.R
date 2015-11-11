e8b29fe81cb50c6701bd95b0a37d4b404a42bbe5
Aura Maria Salazar <auramariasalazar94@gmail.com>
2015-11-10 20:28:50 -0600

    Added summary data medicare connection

---
+++ 01 Data/Medicare_summary.R
@@ -0,0 +1,5 @@
+require("jsonlite")
+require("RCurl")
+require("dplyr")
+medicare <- data.frame(fromJSON(getURL(URLencode('129.152.144.84:5001/rest/native/?query="select * from emp"'),httpheader=c(DB='jdbc:oracle:thin:@129.152.144.84:1521/PDBF15DV.usuniversi01134.oraclecloud.internal', USER='cs329e_alm3657', PASS='orcl_alm3657', MODE='native_mode', MODEL='model', returnDimensions = 'False', returnFor = 'JSON'), verbose = TRUE) ))
+summary(medicare)
